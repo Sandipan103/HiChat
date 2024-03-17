c3cb003865fc2432d520b6b4ef98732edb2945e4
Yogesh Kumar Sai <yogesh01041961@gmail.com>
2024-03-18 00:32:59 +0530

    update main

--- frontend/src/component/ChatComponent/GroupChatBox.jsx
+++ frontend/src/component/ChatComponent/GroupChatBox.jsx
@@ -3,42 +3,42 @@ import axios from "axios";
 import io from "socket.io-client";
 import { server, AuthContext } from "../../context/UserContext";
 
-import SpeedDial from "@mui/material/SpeedDial";
-import SpeedDialIcon from "@mui/material/SpeedDialIcon";
-import SpeedDialAction from "@mui/material/SpeedDialAction";
-import FileCopyIcon from "@mui/icons-material/FileCopyOutlined";
-import SaveIcon from "@mui/icons-material/Save";
-import PrintIcon from "@mui/icons-material/Print";
-import ShareIcon from "@mui/icons-material/Share";
-import Button from "@mui/material/Button";
-import SendIcon from "@mui/icons-material/Send";
-import Input from "@mui/material/Input";
-const ariaLabel = { "aria-label": "description" };
-
-const actions = [
-  { icon: <FileCopyIcon />, name: "Copy" },
-  { icon: <SaveIcon />, name: "Save" },
-  { icon: <PrintIcon />, name: "Print" },
-  { icon: <ShareIcon />, name: "Share" },
-];
+import { FileShareMenu } from "./FileShareMenu";
+import { FileSendPopUp } from "./FileSendPopUp";
+import ZegoCloud from "./ZegoCloud";
+import { ChatTextInput } from "./ChatTextInput";
 
 let socket;
 
-const GroupChatBox = ({ messages, setMessages, myId, selectedChat, setChats, chats}) => {
+const GroupChatBox = ({
+  messages,
+  setMessages,
+  myId,
+  selectedChat,
+  setChats,
+  chats,
+}) => {
   const [messageInput, setMessageInput] = useState("");
   const [file, setFile] = useState(null);
-
-  // const [socket, setSocket] = useState("");
   const [socketConnected, setSocketConnected] = useState(false);
   const [open, setOpen] = React.useState(false);
   const handleOpen = () => setOpen(!open);
   const handleClose = () => setOpen(false);
+  const [popOpen, setPopOpen] = useState(false);
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [selectedType, setSelectedType] = useState(null);
 
   const messagesEndRef = useRef(null);
 
+  const [calleeId, setCalleeId] = useState();
+
+  const handleCloseModal = () => {
+    setPopOpen(false);
+  };
+
   const scrollToBottom = () => {
     if (messagesEndRef.current) {
-      messagesEndRef.current.scrollIntoView({ behavior: 'smooth' });
+      messagesEndRef.current.scrollIntoView({ behavior: "auto" });
     }
   };
 
@@ -56,33 +56,38 @@ const GroupChatBox = ({ messages, setMessages, myId, selectedChat, setChats, cha
 
     socket.emit("join chat", selectedChat._id);
 
-  }, []);
+    const oth = selectedChat.users.find((id) => id != myId);
+    setCalleeId(oth);
+  }, [selectedChat]);
 
   useEffect(() => {
     socket.on("message recieved", (newMessage) => {
       console.log(newMessage);
       if (
         !selectedChat || // if chat is not selected or doesn't match current chat
-        selectedChat._id !== newMessage.chat._id
+        selectedChat._id !== newMessage.chat
       ) {
         // console.log('new message recived : ', newMessage)
         // console.log('selectedChat : ', selectedChat)
-        const updatedChats = chats.map(chat => {
-          if (chat._id === newMessage.chat._id) {
+        const updatedChats = chats.map((chat) => {
+          if (chat._id === newMessage.chat) {
             const cnt = (chat.unreadMsgCount || 0) + 1;
-            return { ...chat, unreadMsgCount : cnt};
+            return {
+              ...chat,
+              unreadMsgCount: cnt,
+              latestMessage: newMessage.content,
+            };
           }
           return chat;
         });
 
-        
-
-        const index = updatedChats.findIndex(chat => chat._id === newMessage.chat._id);
+        const index = updatedChats.findIndex(
+          (chat) => chat._id === newMessage.chat
+        );
         if (index !== -1) {
           const chatWithNewMessage = updatedChats.splice(index, 1)[0];
           updatedChats.unshift(chatWithNewMessage);
         }
-        
         setChats(updatedChats);
       } else {
         setMessages([...messages, newMessage]);
@@ -90,91 +95,73 @@ const GroupChatBox = ({ messages, setMessages, myId, selectedChat, setChats, cha
     });
 
     socket.on("file recieved", (imageData) => {
-      if(imageData){
-      const imgElement = document.createElement("img");
-      imgElement.src = imageData;
+      if (imageData) {
+        const imgElement = document.createElement("img");
+        imgElement.src = imageData;
 
-      // const newMsg = { type: 'img', content: imgElement };
-      setMessages([...messages, imgElement]);
+        // const newMsg = { type: 'img', content: imgElement };
+        setMessages([...messages, imgElement]);
 
-      
-
-      // if (messageHeaderRef.current) {
-      //   messageHeaderRef.current.appendChild(imgElement);
-      // }
-      console.log(imgElement);
-    }
-
-    });
-  });
-
-  const handleMessageInputChange = (e) => {
-    setMessageInput(e.target.value);
-  };
-
-  const handleSendMessage = async () => {
-    try {
-      // socket.emit("new message", {
-      //   newMessage: response.data.newMessage,
-      //   chatUsers: djhasjdhjaskh,
-      // });
-        const response = await axios.post(`${server}/sendChatMessage`, {
-          myId,
-          chatId : selectedChat._id,
-          messageInput,
-        });
-        // console.log(response.data.newMessage);
-        // console.log(response.data.chatUsers);
-        // console.log(messages);
-        setMessageInput("");
-        socket.emit("new message", {
-          newMessage: response.data.newMessage,
-          chatUsers: response.data.chatUsers,
-        });
-        // socket.emit("new message", response.data.newMessage);
-        setMessages([...messages, response.data.newMessage]);
-      } catch (error) {
-        console.error("Error sending message:", error);
+        // if (messageHeaderRef.current) {
+        //   messageHeaderRef.current.appendChild(imgElement);
+        // }
+        console.log(imgElement);
       }
-  }
+    });
+  }, [messages, selectedChat, chats, setChats]);
 
-  
-  const handleFileChange = (e) => {
-    setFile(e.target.files[0]);
+  const handleDeleteMessage = async (messageId) => {
+    try {
+      // await axios.post(`${server}/deletemessage/${messageId}`,{userId});
+      const response = await axios.post(
+        `${server}/deletemessage`,
+        { messageId: messageId, userId: myId },
+        { withCredentials: true }
+      );
+
+      const updatedMessages = messages;
+      setMessages(updatedMessages);
+
+      socket.emit("message deleted", messageId);
+    } catch (error) {
+      console.error("Failed to delete message:", error);
+    }
   };
 
   const handleFileUpload = () => {
+    console.log(Date.now());
     try {
-      if (file) {
+      if (selectedFile) {
         const reader = new FileReader();
 
         reader.onload = async (e) => {
           const fileData = e.target.result;
-          const filename = file.name;
+          const filename = selectedFile.name;
+          const chatId = selectedChat._id;
+
           const data = new FormData();
 
-          const response = await axios.post(`${server}/sendGroupFiles`, {
-            myId,
-            chatId: selectedGroup._id,
-            messageInput : filename,
-          });
+          data.append("file", selectedFile);
+          data.append("myId", myId);
+          data.append("chatId", chatId);
+          data.append("type", selectedType);
+          // data.append("fileUrl", fileUrl);
+          data.append("messageInput", messageInput);
 
-            socket.emit("file", {
-              chatUsers: response.data.chatUsers,
-              filename: filename,
-              fileData: fileData,
-            });
-            setFile(null);
+          const response = await axios.post(`${server}/sendFiles`, data);
+          console.log(response);
+          // socket.emit("file", {
+          //   chatUsers: response.data.chatUsers,
+          //   filename: filename,
+          //   fileData: fileData,
+          // });
+          setFile(null);
         };
-        reader.readAsDataURL(file);
-      
-    }
-
+        reader.readAsDataURL(selectedFile);
+      }
     } catch (error) {
       console.error("Error file sending: ", error);
     }
-  
-   
   };
 
   return (
@@ -182,86 +169,100 @@ const GroupChatBox = ({ messages, setMessages, myId, selectedChat, setChats, cha
       <div className="message-area">
         <div className="message-header">
           {selectedChat.groupName}
+          <ZegoCloud myId={myId} calleeId={calleeId} />
         </div>
-          <ul className="messageArea">
-          {messages && messages.length > 0 ? (
-          <ul className="messageArea">
-            {messages.map((message, index) => (
-              <li
-                key={index}
-                className={
-                    message.sender === myId ? "own-message" : "other-message"
-                }
-              >
-                <div className="message">
-                  <p>{message.sender}</p>
-                  <p>{message.content}</p>
-                </div>
-                <div className="timestamp">
-                  <p className="message-timestamp">
-                    {new Date(message.timestamp).toLocaleString("en-US", {
-                      year: "numeric",
-                      month: "numeric",
-                      day: "numeric",
-                      hour: "numeric",
-                      minute: "numeric",
-                      hour12: true,
-                    })}
-                  </p>
-                </div>
-              </li>
-            ))}
-            <div ref={messagesEndRef}>  </div>
-          </ul>
-        ) : (
-          <p className="messageArea">No chat available</p>
-        )}
-            {/* <div ref={messagesEndRef} /> */}
-          </ul>
-        </div>
+        {!popOpen && (
+          <div className="msg-inner-container">
+            <div className="msg-body">
+              <ul className="messageArea">
+                {messages && messages.length > 0 ? (
+                  <ul className="messageArea">
+                    {messages.map((message, index) => (
+                      <li
+                        key={index}
+                        className={
+                          message.sender === myId
+                            ? "own-message"
+                            : "other-message"
+                        }
+                      >
+                        <div className="message">
+                          <p>{message.sender}</p>
+                          <p>{message.content}</p>
+                          {message.sender === myId && (
+                            <button
+                              onClick={() => handleDeleteMessage(message._id)}
+                              className="delete-message-btn"
+                            >
+                              Delete
+                            </button>
+                          )}
+                        </div>
+                        <div className="timestamp">
+                          <p className="message-timestamp">
+                            {new Date(message.timestamp).toLocaleString(
+                              "en-US",
+                              {
+                                year: "numeric",
+                                month: "numeric",
+                                day: "numeric",
+                                hour: "numeric",
+                                minute: "numeric",
+                                hour12: true,
+                              }
+                            )}
+                          </p>
+                        </div>
+                      </li>
+                    ))}
+                    <div ref={messagesEndRef}> </div>
+                  </ul>
+                ) : (
+                  <p className="messageArea">No chat available</p>
+                )}
+                {/* <div ref={messagesEndRef} /> */}
+              </ul>
+            </div>
+            <div className="message-footer">
+              <FileShareMenu
+                popOpen={popOpen}
+                setPopOpen={setPopOpen}
+                handleCloseModal={handleCloseModal}
+                setSelectedFile={setSelectedFile}
+                setSelectedType={setSelectedType}
+              />
+              <ChatTextInput
+                messageInput={messageInput}
+                setMessageInput={setMessageInput}
+                myId={myId}
+                selectedChat={selectedChat}
+                socket={socket}
+                chats={chats}
+                setChats={setChats}
+                messages={messages}
+                setMessages={setMessages}
+              />
 
-      <div className="message-input">
-        <SpeedDial
-          ariaLabel="SpeedDial tooltip example"
-          sx={{ position: "absolute", bottom: 16, right: 16 }}
-          icon={<SpeedDialIcon />}
-          onClick={handleOpen}
-          open={open}
-        >
-          {actions.map((action) => (
-            <SpeedDialAction
-              key={action.name}
-              icon={action.icon}
-              tooltipTitle={action.name}
-              tooltipOpen
-              onClick={handleClose}
-            />
-          ))}
-        </SpeedDial>
-        <Input
-        //   ref={inputRef}
-          type="text"
-          placeholder="Type Something"
-          value={messageInput}
-          onChange={handleMessageInputChange}
-          inputProps={ariaLabel}
-        />
-        <Button
-          variant="contained"
-          onClick={handleSendMessage}
-          endIcon={<SendIcon />}
-        >
-          Send
-        </Button>
-
-        <div>
+              {/* <div>
           <input type="file" onChange={handleFileChange} />
           <button onClick={handleFileUpload}>Upload File</button>
-        </div>
+        </div> */}
+            </div>
+          </div>
+        )}
+
+        {popOpen && (
+          <FileSendPopUp
+            fileType={selectedType}
+            popOpen={popOpen}
+            handleCloseModal={handleCloseModal}
+            selectedFile={selectedFile}
+            handleFileUpload={handleFileUpload}
+          />
+        )}
       </div>
     </>
   );
 };
-
 
 export default GroupChatBox;
