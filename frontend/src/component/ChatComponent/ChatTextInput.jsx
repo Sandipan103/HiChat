9e5874b9b545838072ff485371050db845ebe040
rohan-algorithm <rohanrao543211@gmail.com>
2024-03-18 07:34:57 +0530

    Offline Msg Feature Added

--- frontend/src/component/ChatComponent/ChatTextInput.jsx
+++ frontend/src/component/ChatComponent/ChatTextInput.jsx
@@ -1,36 +1,54 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { server, AuthContext } from "../../context/UserContext";
-
-
-
 import SendIcon from "@mui/icons-material/Send";
+import DoneIcon from "@mui/icons-material/Done";
 import Paper from "@mui/material/Paper";
 import InputBase from "@mui/material/InputBase";
 import Divider from "@mui/material/Divider";
 import IconButton from "@mui/material/IconButton";
 import AddReactionIcon from "@mui/icons-material/AddReaction";
 
+export const ChatTextInput = ({
+  messageInput,
+  setMessageInput,
+  myId,
+  selectedChat,
+  socket,
+  setChats,
+  chats,
+  setMessages,
+  messages,
+}) => {
+  const [offlineMessages, setOfflineMessages] = useState([]);
+  const [sending, setSending] = useState(false);
 
-export const ChatTextInput = ({messageInput,setMessageInput,myId,selectedChat,socket, setChats,chats,setMessages,messages }) => {
+  useEffect(() => {
+    const storedMessages = localStorage.getItem("offlineMessages");
+    if (storedMessages) {
+      setOfflineMessages(JSON.parse(storedMessages));
+    }
+  }, []);
+
   const handleMessageInputChange = (e) => {
     setMessageInput(e.target.value);
   };
 
   const handleSendMessage = async () => {
     try {
+      setSending(true);
+
       const response = await axios.post(`${server}/sendChatMessage`, {
         myId,
         chatId: selectedChat._id,
         messageInput,
       });
-      // console.log(response.data.newMessage);
-      // console.log(response.data.chatUsers);
-      // console.log(messages);
+
       socket.emit("new message", {
         newMessage: response.data.newMessage,
         chatUsers: response.data.chatUsers,
       });
+
       const updatedChats = chats.map((chat) => {
         if (chat._id === selectedChat._id) {
           return { ...chat, latestMessage: messageInput };
@@ -41,8 +59,22 @@ export const ChatTextInput = ({messageInput,setMessageInput,myId,selectedChat,so
       setChats(updatedChats);
       setMessageInput("");
       setMessages([...messages, response.data.newMessage]);
+
+      setSending(false);
+      // Clear offline messages after sending
+      setOfflineMessages([]);
+      localStorage.removeItem("offlineMessages");
     } catch (error) {
       console.error("Error sending message:", error);
+      const newOfflineMessage = {
+        chatId: selectedChat._id,
+        messageInput,
+      };
+      setOfflineMessages([...offlineMessages, newOfflineMessage]);
+      localStorage.setItem(
+        "offlineMessages",
+        JSON.stringify([...offlineMessages, newOfflineMessage])
+      );
     }
   };
 
@@ -65,7 +97,7 @@ export const ChatTextInput = ({messageInput,setMessageInput,myId,selectedChat,so
           placeholder="Type a message"
           value={messageInput}
           onChange={handleMessageInputChange}
-          inputProps={{ ariaLabel: "Type a message" }}
+          inputProps={{ "aria-label": "Type a message" }}
         />
         <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
         <IconButton
@@ -74,9 +106,13 @@ export const ChatTextInput = ({messageInput,setMessageInput,myId,selectedChat,so
           aria-label="SendIcon"
           onClick={handleSendMessage}
         >
-          <SendIcon />
+          {sending ? <SendIcon /> : <DoneIcon />}
         </IconButton>
       </Paper>
+      {/* Display offline messages */}
+      {offlineMessages.map((offlineMessage, index) => (
+        <div key={index}>{offlineMessage.messageInput}</div>
+      ))}
     </>
   );
 };
